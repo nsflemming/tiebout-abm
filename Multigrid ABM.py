a1e879a48bfe1e4150af374aec92c8840bd4528d
nsflemming <70294403+nsflemming@users.noreply.github.com>
2023-02-28 19:22:08 -0500

    working on agent class

--- Multigrid ABM.py
+++ Multigrid ABM.py
@@ -1,4 +1,4 @@
-'''Nathaniel Flemming 15/2/23'''
+'''Nathaniel Flemming 28/2/23'''
 #  Agent Based Modeling package imports
 from mesa import Model, Agent
 from mesa.time import RandomActivation
@@ -10,27 +10,29 @@ class Resident(Agent):
     def __init__(self, id, model, preference):  # unique id, model,
         super().__init__(id, model)  # super lets you take args from other classes, in this case model
         self.preference = preference
-        self.current_city = None #need to start w. city at current position
+        self.current_city = None #need to start w/ city at current position
 
     def step(self):
         #current_spending = self.current_city.spending_level
-        neighbor_spending = []
+        neighbor_spending = [] #doesn't work if neighbor spending starts as empty
+        current_spending = 0 #doesn't work if variable is unassigned before for loop
         #neighboring cities to iterate over
         for neighbor in self.model.grid.neighbor_iter(self.pos, moore = True):
             if isinstance(neighbor, City) and neighbor.pos == self.pos: #set current city and current spending
-                self.current_city = neighbor
-                current_spending = self.current_city.spending_level
+                self.current_city = neighbor #current city = neighbor with same position
+                current_spending = self.current_city.spending_level #current spending = that city's spending
             if isinstance(neighbor, City):
                 neighbor_spending.append(neighbor.spending_level)
         closest_spending = min(neighbor_spending, key=lambda x: abs(x-self.preference)) #find smallest spending gap
-        if abs(closest_spending-self.preference) <= abs(current_spending-self.preference):
-            candidates = [neighbor for neighbor in self.model.grid.neighbor_iter(self.current_city.pos, moore=True, include_center=True)
+        if abs(closest_spending-self.preference) < abs(current_spending-self.preference): #if there's a city with a smaller gap than the current one...
+            candidates = [neighbor for neighbor in self.model.grid.neighbor_iter(self.pos, moore=True)
                           if isinstance(neighbor, City) and neighbor.spending_level == closest_spending]
             if candidates:
                 new_city = random.choice(candidates)
                 self.model.grid.move_agent(self, new_city.pos)
                 self.current_city = new_city
         self.model.gap += abs(self.current_city.spending_level-self.preference)  # update model, add spending gap to model tally
+
        ''' neighborcities = self.model.grid.neighbor_iter(
             self.pos,
             moore=True)
